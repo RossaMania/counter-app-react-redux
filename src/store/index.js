cba73726d986d6fa81953ca5d56f650af811c886
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-14 16:19:19 -0500

    Imported createSlice. Created state slice with name, initialState, and reducers.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState: initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -19,6 +19,23 @@ const counterSlice = createSlice({
       state.showCounter = !state.showCounter;
     },
   },
+});
+
+const initialAuthState = {
+  isAuthenticated: false
+}
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    }
+  }
 });
 
 // const counterReducer = (state = initialState, action) => {
